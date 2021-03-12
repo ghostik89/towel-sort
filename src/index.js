2390781d26dba07f27b8d2d0cfe92a0f18ff8489
Lady_Proger <facktartar@mail.com>
2021-03-12 20:53:32 +0300

    slove towel-sort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if(matrix){
+    matrix.forEach((element,index) => {
+      if(index % 2 === 1)
+        element.reverse()
+    });
+
+    return [].concat(...matrix)
+  }
+  
+  return []
 }
